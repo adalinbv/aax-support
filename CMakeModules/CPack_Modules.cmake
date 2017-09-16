66606e456e0d51478e2ae2bbd98a51abf52ac749
Erik Hofman <erik@ehofman.com>
2017-09-16 16:23:21 +0200

    Another attempt

--- CMakeModules/CPack_Modules.cmake
+++ CMakeModules/CPack_Modules.cmake
@@ -69,25 +69,20 @@ macro (CMP_QT_LIBRARIES_INSTALL_RULES QTLIBLIST destination comp)
   endif()
 endmacro()
 
-macro(install_qt5_plugin _qt_plugin_name _qt_plugins_var)
-  get_target_property(_qt_plugin_path "${_qt_plugin_name}" LOCATION)
-  if(EXISTS "${_qt_plugin_path}")
-    get_filename_component(_qt_plugin_file "${_qt_plugin_path}" NAME)
-    get_filename_component(_qt_plugin_type "${_qt_plugin_path}" PATH)
-    get_filename_component(_qt_plugin_type "${_qt_plugin_type}" NAME)
-    if(APPLE)
-      set(_qt_plugin_dir "PlugIns")
-    elseif(WIN32)
-      set(_qt_plugin_dir "plugins")
-    endif()
-    set(_qt_plugin_dest "${_qt_plugin_dir}/${_qt_plugin_type}")
-    install(FILES "${_qt_plugin_path}"
-      DESTINATION "${_qt_plugin_dest}"
-      ${COMPONENT})
-    set(${_qt_plugins_var}
-      "${${_qt_plugins_var}};\$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/${_qt_plugin_dest}/${_qt_plugin_file}")
+macro(install_qt5_plugin QTNAME destination comp)
+  get_target_property(QTPLUGINPATH "${QTNAME}" LOCATION)
+  if(EXISTS "${QTPLUGINPATH}")
+    get_filename_component(QTPLUGINFILE "${QTPLUGINPATH}" NAME)
+    get_filename_component(QTPLUGINTYPE "${QTPLUGINPATH}" PATH)
+    get_filename_component(QTPLUGINTYPE "${QTPLUGINTYPE}" NAME)
+    set(QTPLUGINDEST "PlugIns/${QTPLUGINTYPE}")
+    install(FILES "${QTPLUGINPATH}"
+      DESTINATION "${QTPLUGINDEST}"
+      CONFIGURATIONS Release
+      COMPONENT "${comp}")
+
   else()
-    message(FATAL_ERROR "QT plugin ${_qt_plugin_name} not found")
+    message(FATAL_ERROR "QT plugin ${QTNAME} not found")
   endif()
 endmacro()
 
